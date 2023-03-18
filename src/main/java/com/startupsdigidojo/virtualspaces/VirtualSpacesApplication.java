fde95ae6aa4cac84fa174efc38a601f7f0c05a3b
Francesco <francesco.mazzini432@gmail.com>
2023-03-18 10:49:45 +0100

    Prepared MS structure
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

---
+++ src/main/java/com/startupsdigidojo/virtualspaces/VirtualSpacesApplication.java
@@ -0,0 +1,13 @@
+package com.startupsdigidojo.virtualspaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualSpacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualSpacesApplication.class, args);
+	}
+
+}

58c47e1d2a3a2a2f1b35e062ea53cad289f39965
Francesco <francesco.mazzini432@gmail.com>
2023-04-27 15:58:50 +0200

    Allowed CORS
    Signed-off-by: Francesco <francesco.mazzini432@gmail.com>

--- src/main/java/com/startupsdigidojo/virtualspaces/note/application/NoteController.java
+++ src/main/java/com/startupsdigidojo/virtualspaces/note/application/NoteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping(path="/v1/notes")
 
